1bc80d1a9b51917500e9910ae49ea949ca4cebe1
cillustrisimo <carl.g.illustrisimo@gmail.com>
2023-02-27 17:27:57 -0800

    Wrote emailing function for when motion is detected on webcam. Edited notes in main code for self-study purposes.

--- emailing.py
+++ emailing.py
@@ -1,2 +1,31 @@
-def send_email():
-    print("Email was sent")
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+key = 'PASSWORD_EMAILBOT'
+PASSWORD = os.getenv(key)
+SENDER = "automata.python@gmail.com"
+RECEIVER = "automata.python@gmail.com"
+
+
+def send_email(object_image):
+    email_message = EmailMessage()
+    email_message["Subject"] = "Motion detected on webcam"
+    # noinspection PyTypeChecker
+    email_message.set_content("Motion has been detected on webcam. Please see attached image.")
+
+    with open(object_image, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(object_image="images/13.png")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import glob
 from emailing import send_email
 
 video = cv2.VideoCapture(0)
@@ -8,12 +9,14 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
 # the while loop lets the webcam run infinitely until stopped. the frame variable in video read
 # allows us to look at individual webcam frames (image captures) think frames per second.
 while True:
     status = 0
     check, frame = video.read()
+
     # With this I convert all the frames to grayscale, as BGR is more complex data and this allows us
     # to have less to process when detecting changes to the static variable image
     # The gaussian blur is applied to make the image simpler for analysis. 21 denotes the level of blur
@@ -24,19 +27,20 @@ while True:
     if first_frame is None:
         first_frame = gray_frame_gau
 
-    # this essentially begins the video after a change has been detected in the first_frame
+    # this essentially begins the video after a change has been detected in the first_frame (takes difference between two arrays)
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
 
     # this below code essentially checks if a pixel has a value color value above 65,
-    # and then its reassigned the value 255
+    # and then its reassigned the value 255 (making it white)
     # dil frame then dilutes the frame removing more noise
-    thresh_frame = cv2.threshold(delta_frame, 65, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
-    # this finds all the white contours and calculates their area
+    # this finds and approximates all contours of a shape in a given image
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # this makes it so any white displayed less than 5000 pixels is ignored as a false positive
+    # this does so by taking the area of the previously identified contours and compares it to 5000px
     # the bounding rect draws a rectangle around the contours of the area of a true positive detected obj
     for contour in contours:
         if cv2.contourArea(contour) < 5000:
@@ -45,6 +49,11 @@ while True:
         detected = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if detected.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) /2)
+            object_image = all_images[index]
 
     # by doing status list[-2:], we are only looking at the last two items in the status array
     # after which, we limit the amount of emails sent by only sending an email
